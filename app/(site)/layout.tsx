94273615a9a7f8f59c960fafc360b0a9cbfe277f
Yubeen <lybin71@gmail.com>
2023-08-30 15:10:29 +0100

    mobile friendly 1st try

--- app/(site)/layout.tsx
+++ app/(site)/layout.tsx
@@ -18,16 +18,16 @@ export default async function RootLayout({
   const pages = await getPages();
   return (
     <html lang="en">
-      <body className="max-w-4xl mx-auto py-5">
-        <main className="py-7">
+      <body className="max-w-4xl mx-6 py-3 md:mx-auto md:py-5">
+        <main className="py-3 md:py-7">
           <header className="flex items-center justify-between">
             <Link
               href="/"
-              className="bg-gradient-to-r from-orange-400 via-red-500 to-purple-600 bg-clip-text text-transparent text-2xl font-bold"
+              className="bg-gradient-to-r from-orange-400 via-red-500 to-purple-600 bg-clip-text text-transparent text-lg md:text-2xl font-bold"
             >
               Home
             </Link>
-            <div className="flex items-center gap-5 text-sm text-gray-600">
+            <div className="flex items-center gap-5 text-xs md:text-sm text-gray-600">
               {pages.map((page) => (
                 <Link
                   key={page._id}
