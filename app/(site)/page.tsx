3faeff7d7fef63b5b00a3adf8093474bf9296e47
Yubeen <lybin71@gmail.com>
2023-08-31 17:33:18 +0100

    use client + false

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -1,3 +1,4 @@
+"use client";
 import { getProjects } from "@/sanity/sanity-utils";
 import Image from "next/image";
 import Link from "next/link";
--- sanity/config/client-config.ts
+++ sanity/config/client-config.ts
@@ -2,7 +2,7 @@ const config = {
   projectId: "9ypsgm42",
   dataset: "production",
   apiVersion: "2023-03-04",
-  useCdn: true,
+  useCdn: false,
 };
 
 export default config;
